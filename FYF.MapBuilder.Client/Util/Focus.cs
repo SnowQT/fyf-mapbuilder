6b781d12bd050c6b2de029b6fc8be58b9809d8bf
thebma <boum.dev@protonmail.com>
2019-09-11 22:07:09 +0200

    Add basic stuff for NUI.

--- FYF.MapBuilder.Client/Util/Focus.cs
+++ FYF.MapBuilder.Client/Util/Focus.cs
@@ -9,7 +9,7 @@ namespace FYF.MapBuilder.Client
 
         public static void Set(Vector3 position, Vector3 rotation)
         {
-            //NOTE: Convert to a signed angle (0-306 degrees).
+            //NOTE: Convert to a signed angle (0-360 degrees).
             int signedAngle = 180 + (int)rotation.Z;
 
             LockMinimapPosition(position.X, position.Y);
--- server/data/resources/fyf-mapbuilder/__resource.lua
+++ server/data/resources/fyf-mapbuilder/__resource.lua
@@ -1,4 +1,9 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
+files {
+    "nui/index.html"
+}
+
+ui_page "nui/index.html"
 client_script "FYF.MapBuilder.Client.net.dll"
 server_script "FYF.MapBuilder.Server.net.dll"
