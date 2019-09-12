7fd4bce9613faaaf1bcb9d6b7e67fc86b301d9cb
thebma <boum.dev@protonmail.com>
2019-09-12 15:53:26 +0200

    Fix gulp, make TS include declaration files *.d.ts from @types.

--- FYF.MapBuilder.UI/gulpfile.js
+++ FYF.MapBuilder.UI/gulpfile.js
@@ -1,32 +1,43 @@
-/// <binding AfterBuild='clean-copy-build' />
+/// <binding AfterBuild='clean-build-copy' />
 
 const Gulp = require("gulp");
 const GulpClean = require("gulp-clean");
-const Webpack = require("webpack")
 const WebpackConfig = require("./webpack-config");
 const WebpackStream = require("webpack-stream");
 
 const buildPath = "./build/";
 const outputPath = "../server/data/resources/fyf-mapbuilder/nui/";
 
-const ItemsToCopyToOutput = [
+const FilesToCopy = [
     "./build/mapbuilder-bundle.js",
     "./index.html",
-    "./assets/*"
 ];
 
+const FoldersToCopy = [
+    "./assets/**/*"
+]
+
 Gulp.task("build", () => {
+    //Webpack everything together using webpack-stream.
     return WebpackStream(WebpackConfig)
         .pipe(Gulp.dest(buildPath));
 });
 
+
 Gulp.task("clean", () => {
+    //Clean the output and build path.
     return Gulp.src([buildPath, outputPath], { read: false, allowEmpty: true })
         .pipe(GulpClean({ force: true }));
 });
 
-Gulp.task("clean-copy-build", Gulp.series(["clean", "build"], (done) => {
-    Gulp.src(ItemsToCopyToOutput)
+Gulp.task("clean-build-copy", Gulp.series(["clean", "build"], (done) => {
+    //Copy over the script filed.
+    Gulp.src(FilesToCopy)
         .pipe(Gulp.dest(outputPath));
+
+    //Copy over the assets folder.
+    Gulp.src(FoldersToCopy)
+        .pipe(Gulp.dest(outputPath + "assets/"));
+
     done();
 }));
